80677f38cbdbca3a32ec1afd765903c17e4d8da9
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-04-08 23:53:47 +0300

    Add handlers for client

--- internal/adapters/handlers/client.handler.go
+++ internal/adapters/handlers/client.handler.go
@@ -3,26 +3,35 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/tutor-connect-AA/tutor-backend/internal/application/core/domain"
 )
 
 type RegisterClient struct {
-	firstName   string      `json: "firstName"`
-	fathersName string      `json: "fathersName"` //optional
-	phoneNumber string      `json:"string"`
-	email       string      `json: "email"`
-	photo       string      `json: "photo"`
-	role        domain.Role `json: "role"` // should role even exist?
-	rating      float32     `json: "rating"`
+	FirstName   string      `json: "firstName"`
+	FathersName string      `json: "fathersName"` //optional
+	PhoneNumber string      `json:"phoneNumber"`
+	Email       string      `json: "email"`
+	Username    string      `json: "username"`
+	Password    string      `json: "password`
+	Photo       string      `json: "photo"`
+	Role        domain.Role `json: "role"` // should role even exist?
+	Rating      float32     `json: "rating"`
 }
 
 func (adp Adapter) Register(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Post requests only", http.StatusMethodNotAllowed)
+		return
+	}
 	var newClient domain.Client
 	err := json.NewDecoder(r.Body).Decode(&newClient)
 
 	if err != nil {
+		log.Println(err)
 		http.Error(w, "Could not decode json", http.StatusInternalServerError)
 		return
 	}
