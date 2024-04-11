9d5f31ac53463a35e47d0c88e6e4c1eda58505b0
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-04-11 10:49:01 +0300

    Note services for a client

--- internal/ports/api_ports/client.api.go
+++ internal/ports/api_ports/client.api.go
@@ -4,6 +4,16 @@ import (
 	"github.com/tutor-connect-AA/tutor-backend/internal/application/core/domain"
 )
 
+/*A client should have the following services:
+1. Register
+2. Login
+3. Be listed [A list of clients](paginated)
+4. Post a job //Create job service
+5. Update profile
+6. Send hiring request //hiring request service
+7. Profile should be shown in detail based on id
+*/
+
 type APIPort interface {
 	RegisterClient(usr domain.Client) (*domain.Client, error)
 	GetClientById(id string) (*domain.Client, error)
