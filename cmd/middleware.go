afc40d930b7af2d4268ac45bca2dd4c212b7147a
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-04-16 16:01:39 +0300

    Add middleware for authorizaiton

---
+++ cmd/middleware.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/tutor-connect-AA/tutor-backend/internal/utils"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		fmt.Println(token)
+		if token == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Missing authorization header")
+			return
+		}
+		token = token[len("Bearer "):]
+		err := utils.VerifyToken(token)
+
+		if err != nil {
+			http.Error(w, "Not authorized", http.StatusUnauthorized)
+			fmt.Print(err)
+			// fmt.Printf(token)
+			fmt.Fprintf(w, "Invalid token")
+			return
+		}
+		fmt.Fprintf(w, "You are authorized")
+		next.ServeHTTP(w, r)
+	})
+}
