d27c58e92281b2e4405bf8eb672e008c09f07c5a
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-04-22 09:27:12 +0300

    feat: :sparkles: Retrieve job by id and all job list

--- internal/adapters/db/job.db.go
+++ internal/adapters/db/job.db.go
@@ -65,20 +65,59 @@ func (jr JobRepo) CreateJobRepo(job domain.Job) (*domain.Job, error) {
 }
 
 func (jr JobRepo) GetJobByIdRepo(id string) (*domain.Job, error) {
-	var jb *domain.Job
-	res := jr.db.Where("id = ?", id).First(jb)
+	var jb *job_table
+	res := jr.db.Where("id = ?", id).First(&jb)
 	if res.Error != nil {
 		return nil, res.Error
 	}
-	return jb, nil
+	return &domain.Job{
+		Id:                    jb.Id.String(),
+		Title:                 jb.Title,
+		Description:           jb.Description,
+		Posted_By:             jb.Posted_By,
+		Deadline:              jb.Deadline,
+		Region:                jb.Region,
+		City:                  jb.City,
+		Quantity:              jb.Quantity,
+		Grade_Of_Students:     jb.Grade_Of_Students,
+		Minimum_Education:     jb.Minimum_Education,
+		Preferred_Gender:      jb.Preferred_Gender,
+		Contact_Hour_Per_Week: jb.Contact_Hour_Per_Week,
+		Status:                jb.Status,
+		Hourly_Rate_Min:       jb.Hourly_Rate_Min,
+		Hourly_Rate_Max:       jb.Hourly_Rate_Max,
+	}, nil
 }
 
 func (jr JobRepo) GetJobsRepo() ([]*domain.Job, error) {
-	var jbs []*domain.Job
+	var jbs []*job_table
+
+	var jobList []*domain.Job
 
 	res := jr.db.Find(&jbs)
 	if res.Error != nil {
 		return nil, res.Error
 	}
-	return jbs, res.Error
+
+	for _, job := range jbs {
+		oneJob := &domain.Job{
+			Id:                    job.Id.String(),
+			Title:                 job.Title,
+			Description:           job.Description,
+			Posted_By:             job.Posted_By,
+			Deadline:              job.Deadline,
+			Region:                job.Region,
+			City:                  job.City,
+			Quantity:              job.Quantity,
+			Grade_Of_Students:     job.Grade_Of_Students,
+			Minimum_Education:     job.Minimum_Education,
+			Preferred_Gender:      job.Preferred_Gender,
+			Contact_Hour_Per_Week: job.Contact_Hour_Per_Week,
+			Status:                job.Status,
+			Hourly_Rate_Min:       job.Hourly_Rate_Min,
+			Hourly_Rate_Max:       job.Hourly_Rate_Max,
+		}
+		jobList = append(jobList, oneJob)
+	}
+	return jobList, res.Error
 }
