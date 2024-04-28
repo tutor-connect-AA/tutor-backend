d0f36a49b10ea32f63d0d37d10a1f5dae790f32f
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-04-28 14:22:12 +0300

    Add multiple file upload

--- cmd/fileUpload.go
+++ cmd/fileUpload.go
@@ -7,14 +7,23 @@ import (
 
 func FileUploadMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		file, header, err := r.FormFile("photo")
+		photo, photoHeader, err := r.FormFile("photo")
 		if err != nil {
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			http.Error(w, "Could not get photo file from request", http.StatusInternalServerError)
 			return
 		}
-		defer file.Close()
-		ctx := context.WithValue(r.Context(), "filePath", header.Filename)
-		ctx = context.WithValue(ctx, "file", file)
+		defer photo.Close()
+		ctx := context.WithValue(r.Context(), "photoPath", photoHeader.Filename)
+		ctx = context.WithValue(ctx, "photo", photo)
+
+		edu, eduHeader, err := r.FormFile("cv")
+		if err != nil {
+			http.Error(w, "Could not get education credential files", http.StatusInternalServerError)
+			return
+		}
+		defer photo.Close()
+		ctx = context.WithValue(r.Context(), "cvPath", eduHeader.Filename)
+		ctx = context.WithValue(ctx, "cv", edu)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
