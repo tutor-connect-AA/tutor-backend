d8b9e2195751f598652f78898484b664661ff03d
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-04-29 14:53:09 +0300

    feat: :sparkles: fix setting multiple files in the request context

--- cmd/fileUpload.go
+++ cmd/fileUpload.go
@@ -15,15 +15,17 @@ func FileUploadMiddleware(next http.Handler) http.Handler {
 		defer photo.Close()
 		ctx := context.WithValue(r.Context(), "photoPath", photoHeader.Filename)
 		ctx = context.WithValue(ctx, "photo", photo)
+		// fmt.Printf("The uploaded photo header: %v", photo)
 
-		edu, eduHeader, err := r.FormFile("cv")
+		cv, cvHeader, err := r.FormFile("cv")
 		if err != nil {
 			http.Error(w, "Could not get education credential files", http.StatusInternalServerError)
 			return
 		}
-		defer photo.Close()
-		ctx = context.WithValue(r.Context(), "cvPath", eduHeader.Filename)
-		ctx = context.WithValue(ctx, "cv", edu)
+		defer cv.Close()
+		ctx = context.WithValue(ctx, "cvPath", cvHeader.Filename)
+		ctx = context.WithValue(ctx, "cv", cv)
+		// fmt.Printf("The uploaded cv header %v", cv)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
