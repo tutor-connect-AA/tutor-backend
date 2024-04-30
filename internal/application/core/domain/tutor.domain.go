75faac89ba4733925b0b17e9bd6cbfbf5a4a665b
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-04-30 20:18:54 +0300

    feat: :sparkles: Update data type

--- internal/application/core/domain/tutor.domain.go
+++ internal/application/core/domain/tutor.domain.go
@@ -5,30 +5,30 @@ import (
 )
 
 type Tutor struct {
-	Id                       string
-	FirstName                string
-	FathersName              string
-	Email                    string
-	PhoneNumber              string
-	Gender                   Gender
-	Photo                    string
-	Rating                   float32
-	Bio                      string
-	Username                 string
-	Password                 string
-	Role                     Role
-	CV                       string //link to file or blob?
-	HourlyRate               float32
-	Region                   string
-	City                     string
-	Education                Education
-	FieldOfStudy             string //could be an enum?
-	EducationCredential      string //link to file
-	CurrentlyEnrolled        Education
-	ProofOfCurrentEnrollment string //link to file
-	GraduationDate           time.Time
-	PreferredSubjects        string //should be an enum limited to 2
-	PreferredWorkLocation    string // should this exist
-	CreatedOn                time.Time
-	UpdatedOn                time.Time
+	Id                  string
+	FirstName           string
+	FathersName         string
+	Email               string
+	PhoneNumber         string
+	Gender              Gender
+	Photo               string
+	Rating              float32
+	Bio                 string
+	Username            string
+	Password            string
+	Role                Role
+	CV                  string //link to file or blob?
+	HourlyRate          float32
+	Region              string
+	City                string
+	Education           Education
+	FieldOfStudy        string //could be an enum?
+	EducationCredential string //link to file
+	CurrentlyEnrolled   Education
+	// ProofOfCurrentEnrollment string //link to file
+	GraduationDate        time.Time
+	PreferredSubjects     string //should be an enum limited to 2
+	PreferredWorkLocation string // should this exist
+	CreatedOn             time.Time
+	UpdatedOn             time.Time
 }
