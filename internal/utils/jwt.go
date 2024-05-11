93d5d27e34dff8ec3dc2819ee419b991e7b1af6f
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-05-11 11:53:54 +0300

    Change token signing algorithm form JWT

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -8,30 +8,47 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var key, err = GenerateKey() //handle this better
-// var key = []byte("secret-key")
+// var key, err = GenerateKey() //handle this better
+var key = []byte("secret-key")
 
 func Tokenize(id string) (string, error) {
 
-	if err != nil {
-		return "", nil
-	}
+	// if err != nil {
+	// 	return "", nil
+	// }
 
 	// fmt.Println("Tokenizing key is :", key)
 
-	expiration := time.Now().Add(time.Hour * 24)
+	// expiration := time.Now().Add(time.Hour * 24)
 
-	t := jwt.NewWithClaims(jwt.SigningMethodES256,
-		jwt.MapClaims{
-			"id":  id,
-			"exp": expiration.Unix(),
-		})
-	s, err := t.SignedString(key)
+	// t := jwt.NewWithClaims(jwt.Hc,
+	// 	jwt.MapClaims{
+	// 		"id":  id,
+	// 		"exp": expiration.Unix(),
+	// 	})
+	// s, err := t.SignedString(key)
+	// if err != nil {
+	// 	fmt.Print("err1", err)
+	// 	return "", err
+	// }
+	// return s, nil
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["sub"] = "subject"
+	claims["id"] = id
+	claims["exp"] = time.Now().Add(time.Second * 30).Unix()
+
+	// Sign the token with the secret key
+	tokenString, err := token.SignedString(key)
 	if err != nil {
-		fmt.Print("err1", err)
+		fmt.Println("Error signing token:", err)
 		return "", err
 	}
-	return s, nil
+
+	fmt.Println("JWT token:", tokenString)
+	return tokenString, nil
 }
 
 // look deeper into jwt token verification options for better security
@@ -41,11 +58,12 @@ func VerifyToken(tokenString string) (map[string]interface{}, error) {
 	// })
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Check signing method
-		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		// Extract public key from token
-		return &key.PublicKey, nil
+		// return &key.PublicKey, nil
+		return key, nil
 	})
 	// fmt.Println("Verifying key is :", key)
 	if err != nil {
@@ -67,13 +85,13 @@ func GetPayload(r *http.Request) (map[string]string, error) {
 	token := r.Header.Get("Authorization")
 	token = token[len("Bearer "):]
 
-	if err != nil {
-		return payload, err
-	}
+	// if err != nil {
+	// 	return payload, err
+	// }
 
 	claims, err := VerifyToken(token)
 	if err != nil {
-		fmt.Printf("Could not verify token%v", err)
+		fmt.Printf("Could not verify token\n %v ", err)
 		return payload, err
 	}
 	id, ok := claims["id"].(string)
