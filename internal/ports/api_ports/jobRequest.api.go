2a97720c40550f37aa7c25d69e8b262b63cfd96c
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-05-20 21:40:46 +0300

    Create job request ports

---
+++ internal/ports/api_ports/jobRequest.api.go
@@ -0,0 +1,9 @@
+package api_ports
+
+import "github.com/tutor-connect-AA/tutor-backend/internal/application/core/domain"
+
+type JobRequestAPIPort interface {
+	CreateJobRequest(newJob domain.JobRequest) (*domain.JobRequest, error)
+	JobRequestById(id string) (*domain.JobRequest, error)
+	JobRequests() ([]*domain.JobRequest, error)
+}
