dc2ebe638e86b3fb140181e3767b977422091a2e
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-06-08 23:53:44 +0300

    Add change status for request and hiring

--- internal/application/core/domain/jobRequest.domain.go
+++ internal/application/core/domain/jobRequest.domain.go
@@ -10,5 +10,6 @@ type JobRequest struct {
 	Status      JobRequestStatus
 	ClientId    string //id of the client who sent the request
 	TutorId     string // id of the tutor for whom the request is sent
+	TxRef       string
 	CreatedOn   time.Time
 }
