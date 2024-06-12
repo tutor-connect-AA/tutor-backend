64d8ea627d012e172a36cd786d297e5631f3a8ec
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-06-12 12:14:58 +0300

    Add .env

--- config/cloudinary.go
+++ config/cloudinary.go
@@ -1,13 +1,15 @@
 package config
 
 import (
+	"os"
+
 	"github.com/cloudinary/cloudinary-go/v2"
 )
 
 func SetupCloudinary() (*cloudinary.Cloudinary, error) {
-	cldSecret := "uFJhxNAJlCeLqQbSqp8-pV3H7lE"
-	cldName := "dytequc2m"
-	cldKey := "283248446939275"
+	cldSecret := os.Getenv("CLOUDINARY_SECRET")
+	cldName := os.Getenv("CLOUDINARY_NAME")
+	cldKey := os.Getenv("CLOUDINARY_KEY")
 
 	cld, err := cloudinary.NewFromParams(cldName, cldKey, cldSecret)
 	if err != nil {
