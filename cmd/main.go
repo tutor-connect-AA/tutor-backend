070a8db8ba183a76613b932a378e90944f657718
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-06-15 04:19:00 +0300

    Cors again

--- cmd/main.go
+++ cmd/main.go
@@ -26,7 +26,7 @@ func init() {
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
