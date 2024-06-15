daf950ab7c47023c3ae2eca73573f06b30516505
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-06-15 04:07:53 +0300

    cors

--- cmd/main.go
+++ cmd/main.go
@@ -24,6 +24,22 @@ func init() {
 	}
 }
 
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight requests
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	//Login endpoint should be shared by tutor and client?
