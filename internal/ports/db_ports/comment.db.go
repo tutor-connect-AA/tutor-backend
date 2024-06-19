dccef035bca72dff94a49ac517c4d02df5fb3413
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-06-19 04:33:28 +0300

    Minor

--- internal/ports/db_ports/comment.db.go
+++ internal/ports/db_ports/comment.db.go
@@ -5,5 +5,5 @@ import "github.com/tutor-connect-AA/tutor-backend/internal/application/core/doma
 type CommentDBPort interface {
 	CreateCommentRepo(newComment domain.Comment) (*domain.Comment, error)
 	GetCommentByIdRepo(id string) (*domain.Comment, error)
-	GetCommentsRepo() ([]*domain.Comment, error)
+	GetCommentsRepo(tutId string) ([]*domain.Comment, error)
 }
