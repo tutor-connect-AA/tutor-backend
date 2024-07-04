572500df6286b57f35eb3089fa02d1b9bf302874
mahider <mahider3991@gmail.com>
2024-07-05 02:56:05 +0300

    util send email

---
+++ internal/utils/email.go
@@ -0,0 +1,33 @@
+package utils
+
+import(
+	"fmt"
+	"net/smtp"
+)
+
+func SendEmail(to []string, message string) {
+	from := "mahider3991@gmail.com"
+	password := "Maverick2020!"
+  
+	// Receiver email address.
+	// to := []string{
+	//   "sender@example.com",
+	// }
+
+	// to := reveiver
+  
+	// smtp server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+	
+	// Authentication.
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+	
+	// Sending email.
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+	  fmt.Println(err)
+	  return
+	}
+	fmt.Println("Email Sent Successfully!")
+}
