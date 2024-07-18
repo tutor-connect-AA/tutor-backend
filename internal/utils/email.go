8b32445dc55e33c8fda186d4fca46db5f8b44f4c
mahider <95926290+Mahider-T@users.noreply.github.com>
2024-07-18 21:25:42 +0300

    email

--- internal/utils/email.go
+++ internal/utils/email.go
@@ -1,33 +1,33 @@
 package utils
 
-import(
+import (
 	"fmt"
 	"net/smtp"
 )
 
-func SendEmail(to []string, message string) {
+func SendEmail(to []string, message []byte) {
 	from := "mahider3991@gmail.com"
 	password := "Maverick2020!"
-  
+
 	// Receiver email address.
 	// to := []string{
 	//   "sender@example.com",
 	// }
 
 	// to := reveiver
-  
+
 	// smtp server configuration.
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
-	
+
 	// Authentication.
 	auth := smtp.PlainAuth("", from, password, smtpHost)
-	
+
 	// Sending email.
 	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
 	if err != nil {
-	  fmt.Println(err)
-	  return
+		fmt.Println(err)
+		return
 	}
 	fmt.Println("Email Sent Successfully!")
 }
